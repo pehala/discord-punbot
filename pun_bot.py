79d7396d1621fb7156d3c7a2308b70f1436b0e5d
TheSprtCz <halapetr@selfnet.cz>
2019-10-03 22:18:21 +0200

    Add command to see source of the puns

--- pun_bot.py
+++ pun_bot.py
@@ -10,11 +10,16 @@ class PunBot(Plugin):
     def __init__(self, bot, config):
         super().__init__(bot, config)
         bot.config.commands_require_mention = False
-        self.pattern = re.compile('(?<=&quot;).*(?=&quot)')
+        self.pun_pattern = re.compile(r'(?<=&quot;).*(?=&quot)')
 
     # Which includes command argument parsing
     @Plugin.command('pun!')
     def on_echo_command(self, event):
-        r = requests.get('https://www.punoftheday.com/cgi-bin/arandompun.pl')
-        msg = self.pattern.findall(r.text)[0]
+        request = requests.get('https://www.punoftheday.com/cgi-bin/arandompun.pl')
+        text = request.text
+        msg = self.pun_pattern.findall(text)[0]
         event.msg.reply(unescape(msg))
+
+    @Plugin.command('punsource!')
+    def on_echo_command(self, event):
+        event.msg.reply("https://www.punoftheday.com/")
